9841598c02aa7234feb193f2c4fca03f09e83cf8
Fit <fitriyantip94@gmail.com>
2025-08-28 15:36:36 +0900

    first commit

---
+++ fit_project/bin/fit_project.dart
@@ -0,0 +1,5 @@
+import 'package:fit_project/fit_project.dart' as fit_project;
+
+void main(List<String> arguments) {
+  print('Hello world: ${fit_project.calculate()}!');
+}
---
+++ fit_project/test/fit_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:fit_project/fit_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
